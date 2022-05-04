f7272052e9201ecfa617fd03dc76183215bf57ab
Aida-bn <bantezea@gmail.com>
2022-05-04 16:56:52 +0300

    today

--- js/index.js
+++ js/index.js
@@ -1,28 +1,16 @@
-// !! IMPORTANT README:
+let firstName = document.getElementById('fname').value;
+console.log(firstName);
+let lastName = document.getElementById('lname').value;
+console.log(lastName);
+let phoneNumber =  document.getElementById('pno').value;
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+let gender = document.querySelector('input[name="radios"]:checked').value;
+console.log(gender);
 
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
-"use strict";
+let selection = [];
+let choosed = document.getElementsByName('selection');
+for(let i = 0; i<choosed.length; i++){
+  if(choosed[i].checked){
+    selected.push(choosed[i].value);
+  }
+}
